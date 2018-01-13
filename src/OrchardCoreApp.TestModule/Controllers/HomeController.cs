203f54ea73873b4d85d861fccb846e86ec130bcd
David Chen <davidchen@d-techpro.net>
2018-01-13 20:42:51 +0800

    Added

---
+++ src/OrchardCoreApp.TestModule/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrchardCoreApp.TestModule.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/OrchardCoreApp.TestModule/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace OrchardCoreApp.TestModule
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaRoute("TestModuleDefault", "TestModule", "", new { controller = "Home", action = "Index" });
+        }
+    }
+}
